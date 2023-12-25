1393cd057d7eaf45f1680b89a07b8457dd1fec3a
tizarhasan <tizarhasan@Zar-MacBook-Pro.local>
2023-12-25 19:12:09 +0700

    contact form to spreadsheet

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// init Isotope
+// init Isotope //
 var $grid = $('.collection-list').isotope({
   // options
 });
-// filter items on button click
+// filter items on button click //
 $('.filter-button-group').on( 'click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
   resetFilterBtns();
@@ -16,3 +16,14 @@ function resetFilterBtns(){
     $(this).removeClass('active-filter-btn');
   });
 }
+
+// contact form //
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyE60ZfS8s9zs5uheNDteVMfG_N4DIs0zzzZMClm7gzyBO86ySN74Erc_ab-S86XnzZbA/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
